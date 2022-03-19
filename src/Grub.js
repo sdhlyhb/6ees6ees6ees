e407f925c8c9513d675791e329e48e6eb657f229
Danting Huang <serena.huangdanting@gmail.com>
2022-03-19 01:11:03 -0400

    Complete 6ees6ees6ees

--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,19 @@
+/*
+ an age property that is set to 0
+ a color property that is set to pink
+ a food property that is set to jelly
+ an eat method that returns 'Mmmmmmmmm jelly'
+*/
+
 class Grub {
+  constructor () {
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+  }
+
+  eat() {
+    return 'Mmmmmmmmm jelly';
+  }
+
 };
